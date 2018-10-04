d2772834d26273ec036ba4467bc6d256fe885aeb
Aspen <zebington@gmail.com>
2018-10-04 15:36:20 +0100

    added checks to make sure there is at least one student and one sweet.

--- sdd/practicals/two/give_sweets.py
+++ sdd/practicals/two/give_sweets.py
@@ -1,6 +1,14 @@
 number_of_pupils = int(input("How many pupils are there? "))
 number_of_sweets = int(input("How many sweets are there? "))
 
+while number_of_pupils <= 0:
+    print("You can't give something to no one.")
+    number_of_pupils = int(input("How many pupils are there? "))
+
+while number_of_sweets <= 0:
+    print("You can't give them nothing.")
+    number_of_sweets = int(input("How many sweets are there? "))
+
 print("Sweets per pupil: " + str(number_of_sweets // number_of_pupils))
 print("Sweets left over: " + str(number_of_sweets % number_of_pupils))
 
