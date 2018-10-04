6d0f622bfdb6b8c5df57c8964aa618bfebd77b74
Aspen <zebington@gmail.com>
2018-10-04 15:33:21 +0100

    let user choose number of sweets and pupils

--- sdd/practicals/two/give_sweets.py
+++ sdd/practicals/two/give_sweets.py
@@ -1,5 +1,5 @@
-number_of_pupils = 16
-number_of_sweets = 47
+number_of_pupils = int(input("How many pupils are there? "))
+number_of_sweets = int(input("How many sweets are there? "))
 
 print("Sweets per pupil: " + str(number_of_sweets // number_of_pupils))
 print("Sweets left over: " + str(number_of_sweets % number_of_pupils))
