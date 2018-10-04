fff971d89d84f31cbcac86b8e6ffaf1eb19314ec
Aspen <zebington@gmail.com>
2018-10-04 15:30:28 +0100

    practical 2 task 6 logbook evidence

---
+++ sdd/practicals/two/give_sweets.py
@@ -0,0 +1,6 @@
+number_of_pupils = 16
+number_of_sweets = 47
+
+print("Sweets per pupil: " + str(number_of_sweets // number_of_pupils))
+print("Sweets left over: " + str(number_of_sweets % number_of_pupils))
+
