6d10fd4e5be334ab79dd0db486bd1179cf732d99
Aspen <zebington@gmail.com>
2018-10-11 15:14:01 +0100

    added comments to give_sweets script

--- sdd/practicals/two/give_sweets.py
+++ sdd/practicals/two/give_sweets.py
@@ -1,4 +1,13 @@
+"""
+Gets the number of sweets a teacher has, and tells them how many to give each pupil to split them evenly.
+
+AT
+04-10-18
+"""
+
+
 def get_input(thing):
+    """Ask the user to enter a whole number of things and return it"""
     while True:
         response = input("How many %s are there? " % thing)
         try:
@@ -8,6 +17,7 @@ def get_input(thing):
 
 
 def get_value(thing):
+    """Get a positive non-zero value of things from a user and return it"""
     response = get_input(thing)
     while response <= 0:
         print("Please enter a value greater than 0.")
@@ -15,9 +25,11 @@ def get_value(thing):
     return response
 
 
+# Get input
 number_of_pupils = get_value("pupils")
 number_of_sweets = get_value("sweets")
 
+# Print output
 print("Sweets per pupil: " + str(number_of_sweets // number_of_pupils))
 print("Sweets left over: " + str(number_of_sweets % number_of_pupils))
 
