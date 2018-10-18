85afad493ece5eb6f8a37e0971028ce4430a7342
Aspen <zebington@gmail.com>
2018-10-18 15:58:34 +0100

    added check if main

--- sdd/practicals/four/price_analyzer.py
+++ sdd/practicals/four/price_analyzer.py
@@ -18,18 +18,19 @@ def get_input(prompt):
             print("Please enter a valid price, eg 20p.")
 
 
-# Get 5 prices and store them in prices.
-prices = []
-for i in range(1, 6):
-    prices.append(get_input(i))
+if __name__ == "__main__":
+    # Get 5 prices and store them in prices.
+    prices = []
+    for i in range(1, 6):
+        prices.append(get_input(i))
 
-# Calculate output variables.
-total = sum(prices)
-average = total / len(prices)
-highest = max(prices)
-lowest = min(prices)
+    # Calculate output variables.
+    total = sum(prices)
+    average = total / len(prices)
+    highest = max(prices)
+    lowest = min(prices)
 
-# Output the results to the user.
-print("\nTotal price: {0}p\nAverage price: {1}p\nHighest price: {2}p\nLowest price: {3}p".format(
-    total, average, highest, lowest
-))
+    # Output the results to the user.
+    print("\nTotal price: {0}p\nAverage price: {1}p\nHighest price: {2}p\nLowest price: {3}p".format(
+        total, average, highest, lowest
+    ))
