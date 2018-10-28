d50504e8870afaae655f2db4337308186c9a911f
Aspen <zebington@gmail.com>
2018-10-28 18:19:52 +0000

    added random password generator

--- sdd/practicals/five/passwd.py
+++ sdd/practicals/five/passwd.py
@@ -5,12 +5,16 @@ AT
 28/10/18
 """
 
+import string
+import random
+
 
 def get_string(prompt):
     return input("Enter {0}: ".format(prompt))
 
 
-if __name__ == "__main__":
+def get_password():
+    """Prompts the user to enter a new password until it passes given criteria, and prints password changed."""
     username = get_string("username")
     student_id = get_string("student ID")
 
@@ -33,3 +37,23 @@ if __name__ == "__main__":
             print("Password changed.")
             break
         print()
+
+
+def random_password():
+    characters = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+    password = ''
+    for i in range(0, 16):
+        password += random.choice(characters)
+
+    print("Generated password: " + password)
+
+
+if __name__ == "__main__":
+    mode = ''
+    while mode not in ['r', 'c']:
+        mode = input("Would you like a (r)andom or (c)ustom password? ")[0]
+
+    if mode == 'c':
+        get_password()
+    else:
+        random_password()
