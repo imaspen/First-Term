40716107cbf54566200a68a2396ff79e148ed012
Aspen <zebington@gmail.com>
2018-11-15 15:26:21 +0000

    created is email checker

--- sdd/practicals/eight/functions.py
+++ sdd/practicals/eight/functions.py
@@ -6,6 +6,7 @@
 
 """
 import random
+import re
 
 __author__ = "Aspen Thompson"
 __email__ = "u1862679@hud.ac.uk"
@@ -16,6 +17,16 @@ def random_answer():
     """Returns a random string from a list."""
     ANSWERS = ['Yes', 'No', 'Maybe', 'Ni!']
     return random.choice(ANSWERS)
+
+
+def is_email(candidate):
+    """
+    Checks a string to check if it is a valid email address.
+    Parameters: potential email
+    Returns: True if email, False otherwise
+    """
+    email_check = re.compile("\A[\w_.]+@[\w.]+\.[a-zA-Z]+\Z")
+    return email_check.match(candidate)
 
 
 if __name__ == "__main__":
